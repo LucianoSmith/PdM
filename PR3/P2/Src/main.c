24e9b614b58d125f6048a10c808809ecf9d3a675
Luciano M. Smith <92652030+LucianoSmith@users.noreply.github.com>
2023-07-10 08:52:41 -0300

    Add files via upload

---
+++ PR3/P2/Src/main.c
@@ -0,0 +1,70 @@
+/*
+  ******************************************************************************
+  * @title   	Practica 3 - Ejercicio 1
+  * @author  	Luciano M. Smith
+  * @brief		Programa que parpadea leds a distintas velocidades sin bloquear
+  * @hardware 	Nucleo-F429ZI
+  ******************************************************************************
+  */
+
+#include "main.h"
+#include "API_delay.h"		// modulo con las funciones de delay
+
+// tiempos de parpadeo
+
+#define timeLed 200
+
+// inicio de estructuras de delay
+
+delay_t dLed;
+
+// estructura principal
+
+int main(void)
+{
+  // inicialización de la capa de abstracción de hardware
+
+  HAL_Init();
+
+  // inicialización de los delays
+
+  delayInit(&dLed, timeLed);
+
+  int i = 0;			// control de la secuencia de leds
+  bool enc = false;		// indicador de led encendido
+
+  // inicialización de los leds on-board
+
+  BSP_LED_Init(LED1);
+  BSP_LED_Init(LED2);
+  BSP_LED_Init(LED3);
+
+  // bucle infinito
+
+  while (1) {
+
+	  if (delayRead(&dLed)) {
+		  if (enc) {
+
+			  BSP_LED_Off(i);				// apaga el led i
+
+			  enc = false;
+
+			  i++;							// va al siguiente led
+			  if (i>2) i=0;
+
+		  } else {
+
+			  BSP_LED_On(i);				// enciende el led i
+			  enc = true;
+
+		  }
+	  }
+
+
+  }
+
+}
+
+
+
